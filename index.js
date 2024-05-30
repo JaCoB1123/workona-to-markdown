1b6362e1f0a160df1f413f3acc45ad8b12027555
Jan Bader <c.github@jan.javil.eu>
2024-05-30 20:51:10 +0200

    handle archived workspaces

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 try {
 	const text = fs.readFileSync('./userData.json', 'utf8');
 	const data = JSON.parse(text);
-	for(let workspace of data.Workspaces) {
+	for(let workspace of [...data.Workspaces, {workspaces: data["Archived Workspaces"], title: "Archived Workspaces"}]) {
 		console.log("# " + workspace.title);
 		for(let project of workspace.workspaces) {
 			console.log("## " + project.title);
