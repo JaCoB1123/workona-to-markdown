b2b2063f44400ff4b7201817ea56f8b927c70551
Jan Bader <c.github@jan.javil.eu>
2024-05-30 20:40:01 +0200

    Handle more elements and save not handled data to new file

--- index.js
+++ index.js
@@ -10,9 +10,45 @@ try {
 
 			for(let tab of project.tabs) {
 				console.log("- [" + tab.title + "](" + tab.url + ")")
+				delete tab.title
+				delete tab.url
 			}
+			project.tabs = project.tabs.filter(tab => Object.keys(tab).length)
+			if(!project.tabs.length)
+				delete project.tabs
+
+			for(let notes of project.notes) {
+				console.log("### " + notes.title)
+				for(let note of notes.notes) {
+					if(!note.title)
+						delete note.title
+					if(!note.attachments.length)
+						delete note.attachments
+
+					console.log("#### " + note.title)
+					if(!note.description)
+						delete note.description
+					else {
+						for(let line of note.description.lines) {
+							console.log(line)
+						}
+						delete note.description.lines
+					}
+					delete note.title
+				}
+				notes.notes = notes.notes.filter(note => Object.keys(note).length)
+				if(!notes.notes.length)
+					delete notes.notes
+				delete notes.title
+			}
+			project.notes = project.notes.filter(note => Object.keys(note).length)
+
+			if(!project.notes.length)
+				delete project.notes
 		}
 	}
+	const leftovers = JSON.stringify(data, null, "\t");
+	fs.writeFileSync('./leftover.json', leftovers);
 } catch (err) {
   console.error(err);
 }
